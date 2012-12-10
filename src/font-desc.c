e2dbc3e9db7514e16ad3c54db91915e475a11e66
Eyal Lotem <eyal.lotem@gmail.com>
2012-12-10 03:11:54 +0200

    font-desc instead of font-manager

--- src/font-desc.c
+++ src/font-desc.c
@@ -1,6 +1,8 @@
 #include "font-desc.h"
 
 #include <stdio.h>
+#include <string.h>
+
 #ifdef USE_FONT_CONFIG
 #  if !defined(_WIN32) && !defined(_WIN64)
 #    include <fontconfig/fontconfig.h>
@@ -31,25 +33,25 @@ font_desc_find_filename( const font_desc_t *desc )
     }
     FcInit();
     FcPattern *pattern = FcPatternCreate();
-    FcPatternAddDouble( pattern, FC_SIZE, size );
+    FcPatternAddDouble( pattern, FC_SIZE, desc->size );
     FcPatternAddInteger( pattern, FC_WEIGHT, weight );
     FcPatternAddInteger( pattern, FC_SLANT, slant );
     FcPatternAddString( pattern, FC_FAMILY, (FcChar8*) desc->family );
     FcConfigSubstitute( 0, pattern, FcMatchPattern );
     FcDefaultSubstitute( pattern );
-    FcResult result;
-    FcPattern *match = FcFontMatch( 0, pattern, &result );
+    FcResult dummy;
+    FcPattern *match = FcFontMatch( 0, pattern, &dummy );
     FcPatternDestroy( pattern );
 
     if ( !match ) {
-        fprintf( stderr, "fontconfig error: could not match family '%s'", family );
+        fprintf( stderr, "fontconfig error: could not match family '%s'", desc->family );
         return 0;
     }
     FcValue value;
     FcResult result = FcPatternGet( match, FC_FILE, 0, &value );
     FcPatternDestroy( match );
     if ( result ) {
-        fprintf( stderr, "fontconfig error: could not match family '%s'", family );
+        fprintf( stderr, "fontconfig error: could not match family '%s'", desc->family );
         return 0;
     }
     return strdup( (char *)(value.u.s) );
